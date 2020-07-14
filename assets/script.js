08b15596080b9caeaacaa00adfb0a59917c2ac51
HuddyMeraBuddy <mishramanvendra76@gmail.com>
2020-07-15 01:43:44 +0530

    final html changes

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
 var $jq = jQuery.noConflict();
-$jq(document).ready(function(){
+$jq(document).ready(function () {
 
-$jq('.post-wraper').slick({
+  $jq('.post-wraper').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    nextArrow:$jq('.next'),
-    prevArrow:$jq('.prev'),
+    nextArrow: $jq('.next'),
+    prevArrow: $jq('.prev'),
     responsive: [
       {
         breakpoint: 1024,
@@ -39,3 +39,21 @@ $jq('.post-wraper').slick({
   });
 
 });
+
+
+ClassicEditor.create( document.querySelector( '#editor' ), {
+        toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+        heading: {
+            options: [
+                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+                { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+                { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' }
+            ]
+        }
+    } )
+    .catch( error => {
+        console.log( error );
+    } );
+
